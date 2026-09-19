e6da15379315b77503997a04546117eca2a64425
DJIflash <107016143+DJIflash3107@users.noreply.github.com>
2026-09-19 17:21:07 +0700

    initial commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className='text-red-500 text-xl m-125 font-bold'>
+            Hello World!!!!
+        </div>
+    );
+}
+
+export default Landing;
